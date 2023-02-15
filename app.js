77878543a3ef1de6066e993a028de522e31d5b08
LuiMar11 <64877820+LuiMar11@users.noreply.github.com>
2023-02-14 21:45:55 -0500

    :3

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+})
+
+app.get('/suma/:num1/:num2', function (req, res) {
+  var num1 = parseFloat(req.params.num1);
+  var num2 = parseFloat(req.params.num2);
+  var suma = (num1 + num2).toString();
+  res.send(suma);
+})
+
+app.get('/resta/:num1/:num2', function (req, res) {
+  var num1 = parseFloat(req.params.num1);
+  var num2 = parseFloat(req.params.num2);
+  var resta = (num1 - num2).toString();
+  res.send(resta);
+})
+
+app.get('/multiplicacion/:num1/:num2', function (req, res) {
+  var num1 = parseFloat(req.params.num1);
+  var num2 = parseFloat(req.params.num2);
+  var mutiplicacion = (num1 * num2).toString();
+  res.send(mutiplicacion);
+})
+
+app.get('/division/:num1/:num2', function (req, res) {
+  var num1 = parseFloat(req.params.num1);
+  var num2 = parseFloat(req.params.num2);
+  var division = (num1 / num2).toString();
+  res.send(division);
+})
+
+app.listen(3000, function () {
+  console.log('Server online');
+})
